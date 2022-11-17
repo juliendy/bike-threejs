24f4b941ab2eb334acfd36dd7dd4f937e1164ee5
juliendy <julien.yasar@gmail.com>
2022-11-17 22:08:54 +0100

    chore: importing & loading assets

--- Experience/World/Bike.js
+++ Experience/World/Bike.js
@@ -5,11 +5,17 @@ export default class Bike {
     constructor() {
         this.experience = new Experience();
         this.scene = this.experience.scene;
+        this.resources = this.experience.resources;
+        this.bike = this.resources.items.bike;
+        this.actualBike = this.bike.scene;
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-        this.scene.add(cube);
+        console.log(this.bike);
+        console.log(this.actualBike);
+        this.setModel();
+    }
+
+    setModel() {
+        this.scene.add(this.actualBike);
     }
 
     resize() {}
